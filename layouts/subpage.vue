6bbc01f8f3b7c58fcdf27e7201a07f7105303663
Cuke7 <cuke.dev@mail.com>
2021-10-24 18:38:31 +0200

    first commit

---
+++ layouts/subpage.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary">
+      <v-btn icon @click="hasHistory() ? $router.go(-1) : $router.push('/')">
+        <v-icon color="white">
+          mdi-arrow-left
+        </v-icon>
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon @click="share">
+        <v-icon color="white">
+          mdi-share-variant
+        </v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <Nuxt></Nuxt>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "subpage",
+  data: () => ({
+    //
+  }),
+  methods: {
+    share() {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "Quaesto",
+            text: "",
+            url: window.location.href
+          })
+          .then(() => console.log("Shared!"))
+          .catch(error => console.log("Error sharing", error));
+      }
+    },
+
+    hasHistory() {
+      return window.history.length > 2;
+    }
+  }
+};
+</script>
