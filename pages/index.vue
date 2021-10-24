b031588a88b809c4a81b687c82e970f4b0fd9015
Cuke7 <cuke.dev@mail.com>
2021-10-24 18:50:23 +0200

    Better performances

--- pages/index.vue
+++ pages/index.vue
@@ -12,11 +12,15 @@
         hide-details
         autocomplete="off"
       ></v-text-field>
-
-      <SearchResults
-        :searchResults="searchResults"
-        :searchQuery="searchQuery"
-      ></SearchResults>
+      <div class="px-4">
+        <SearchResults
+          :searchResults="searchResults"
+          :searchQuery="searchQuery"
+        ></SearchResults>
+      </div>
+      <v-snackbar v-model="snackbar" timeout="1000">
+        Recherche en cours...
+      </v-snackbar>
     </v-col>
   </v-row>
 </template>
@@ -31,12 +35,65 @@ require("lunr-languages/lunr.fr")(lunr);
 
 export default {
   mounted() {
-    // console.log(this.cec[1]);
+    let documents = [];
+
+    // Build Lunr index
+    for (let p = 0; p < cec.length; p++) {
+      let partie = cec[p];
+      // console.log(partie);
+      for (let s = 2; s < partie.length; s++) {
+        let section = partie[s];
+        // console.log(section);
+        for (let c = 2; c < section.length; c++) {
+          let chapitre = section[c];
+          // console.log(chapitre);
+          for (let a = 2; a < chapitre.length; a++) {
+            let article = chapitre[a];
+            // console.log(article);
+            for (let para = 2; para < article.length; para++) {
+              let paragraphe = article[para];
+              // console.log(paragraphe);
+              for (let r = 2; r < paragraphe.length; r++) {
+                let reference = paragraphe[r];
+                // console.log(reference);
+
+                // let lignes = reference.reduce((a, b) => {
+                //   return a + " " + b;
+                // });
+
+                for (let l = 0; l < reference.length; l++) {
+                  let ligne = reference[l];
+                  let id = [p, s, c, a, para, r, l];
+                  documents.push({
+                    id: id,
+                    text: ligne
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    let idx = lunr(function() {
+      this.use(lunr.fr);
+      this.ref("id");
+      this.field("text");
+      // this.metadataWhitelist = ["position"];
+
+      documents.forEach(function(doc) {
+        this.add(doc);
+      }, this);
+    });
+    this.idx = idx;
   },
   data: () => ({
     cec: cec,
     searchQuery: "Immaculée conception",
-    searchResults: []
+    searchResults: [],
+    snackbar: false,
+    idx: null
   }),
   methods: {
     search() {
@@ -49,60 +106,9 @@ export default {
     }
   },
   computed: {
-    idx() {
-      let documents = [];
-
-      // Build Lunr index
-      for (let p = 0; p < cec.length; p++) {
-        let partie = cec[p];
-        // console.log(partie);
-        for (let s = 2; s < partie.length; s++) {
-          let section = partie[s];
-          // console.log(section);
-          for (let c = 2; c < section.length; c++) {
-            let chapitre = section[c];
-            // console.log(chapitre);
-            for (let a = 2; a < chapitre.length; a++) {
-              let article = chapitre[a];
-              // console.log(article);
-              for (let para = 2; para < article.length; para++) {
-                let paragraphe = article[para];
-                // console.log(paragraphe);
-                for (let r = 2; r < paragraphe.length; r++) {
-                  let reference = paragraphe[r];
-                  // console.log(reference);
-
-                  // let lignes = reference.reduce((a, b) => {
-                  //   return a + " " + b;
-                  // });
-
-                  for (let l = 0; l < reference.length; l++) {
-                    let ligne = reference[l];
-                    let id = [p, s, c, a, para, r, l];
-                    documents.push({
-                      id: id,
-                      text: ligne
-                    });
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-
-      let idx = lunr(function() {
-        this.use(lunr.fr);
-        this.ref("id");
-        this.field("text");
-        // this.metadataWhitelist = ["position"];
-
-        documents.forEach(function(doc) {
-          this.add(doc);
-        }, this);
-      });
-      return idx;
-    }
+    // idx() {
+    //   return idx;
+    // }
   }
 };
 </script>
