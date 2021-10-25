0492c4d67493dffbe67c23bde3c6488d03e73006
Cuke7 <cuke.dev@mail.com>
2021-10-25 18:46:51 +0200

    TEST

---
+++ components/Introduction.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-dialog v-model="dialog" transition="dialog-top-transition">
+    <template v-slot:activator="{ on, attrs }">
+      <v-chip color="primary" outlined v-bind="attrs" v-on="on">
+        Introduction
+      </v-chip>
+    </template>
+    <v-card>
+      <v-card-title>
+        Introduction
+      </v-card-title>
+      <v-card-text class="pb-0">
+        <div
+          v-for="(ligne, index) in intro"
+          :key="index"
+          class="my-2 text-justify"
+        >
+          <div class="text-body-1">
+            {{ ligne }}
+          </div>
+        </div>
+      </v-card-text>
+      <v-card-actions class="pa-4">
+        <v-spacer> </v-spacer>
+        <v-btn color="primary" outlined @click="dialog = false">
+          Fermer
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ["intro"],
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
---
+++ components/Navigation.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-row justify="center">
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <v-chip color="primary" outlined v-bind="attrs" v-on="on">
+          Navigation
+        </v-chip>
+      </template>
+      <v-card>
+        <v-toolbar dark color="primary">
+          <v-btn icon @click="dialog = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>Table des matières</v-toolbar-title>
+        </v-toolbar>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  }
+};
+</script>
